3d9ac931a5d078c473d0ccb46634a6fdc7d51542
Bruno Panuto <bl.panuto@gmail.com>
2016-01-15 01:18:28 -0200

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Writing Web Applications in Golang
+
+It's tons of fun. And productive!
+
+If you're stuck on those last assignments in the [Writing a Web Application](https://golang.org/doc/articles/wiki/)
+and want to take a peek at how I did it, feel free.
+
+
---
+++ wiki.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"html/template"
+    "regexp"
+	"io/ioutil"
+	"net/http"
+)
+
+type Page struct {
+	Title string
+    Body  []byte
+    DisplayBody template.HTML
+}
+
+type titleHandlerFunc func(http.ResponseWriter, *http.Request, string)
+
+var templates = template.Must(template.ParseGlob("tmpl/*.html"))
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+var pageName = regexp.MustCompile(`\[([a-zA-Z0-9]+)\]`)
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile("data/" + filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile("data/" + filename)
+	if err != nil {
+		return nil, err
+	}
+    page := &Page{Title: title, Body: body}
+    page.DisplayBody = template.HTML(pageName.ReplaceAllFunc(page.Body, func(newBody []byte) []byte {
+        matched := pageName.FindStringSubmatch(string(newBody))
+        return []byte(`<a href="/view/` + matched[1] + `">`+ matched[1] + "</a>")
+    }))
+	return page, nil
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	err := templates.ExecuteTemplate(w, tmpl + ".html", p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+	renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
+	body := r.FormValue("body")
+    p := &Page{Title: title, Body: []byte(body)}
+    err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+func makeHandler (fn titleHandlerFunc) http.HandlerFunc {
+    return func (w http.ResponseWriter, r *http.Request) {
+        m := validPath.FindStringSubmatch(r.URL.Path)
+        if m == nil {
+             http.NotFound(w, r)
+             return
+        }
+        fn(w, r, m[2])
+    }
+}
+
+func main() {
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/edit/", makeHandler(editHandler))
+	http.HandleFunc("/save/", makeHandler(saveHandler))
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+        http.Redirect(w, r, "/view/FrontPage", http.StatusFound)
+    })
+	http.ListenAndServe(":8080", nil)
+}
